d23a662a3bc985b9abf7611b15384973668eba2f
Nadezhda <niarygin@student.hive.fi>
2024-04-25 15:51:30 +0300

    add draft for atoi and draft tests

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niarygin <niarygin@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 15:35:16 by niarygin          #+#    #+#             */
+/*   Updated: 2024/04/25 15:35:19 by niarygin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] && ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' '))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * sign);
+}
---
+++ test_atoi.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+int main(void)
+{
+	//Positive Integer
+	//Negative Integer
+	//Leading Spaces
+	//Trailing Spaces
+	//Non-Numeric Characters
+	//Mixed Spaces and Non-Numeric Characters
+	//Empty String
+	//String with Only Spaces
+	//String with Only a Sign
+	//String with a Sign and Non-Numeric Characters
+}
