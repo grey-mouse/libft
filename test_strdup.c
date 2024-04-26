0770e3bbcf29c729bc7af9998ec1e66505eee280
Nadezhda <niarygin@student.hive.fi>
2024-04-26 13:42:49 +0300

    add empty string check in test_strdup

--- test_strdup.c
+++ test_strdup.c
@@ -26,4 +26,24 @@ int	main(void)
     printf("AR:Duplicated string: %s\n", target1);
 	printf("AR: dup addr |%p|\n", target1);
     free(target1);
+
+	//Empty String Test
+	char source1[] = "";
+    char *target2 = strdup(source1);
+    printf("src: %s | address: %p\n", source1, source1);
+	if (target2 == NULL) {
+        printf("ER:Memory allocation failed.\n");
+        return 1;
+    }
+    printf("ER:Duplicated string: %s\n", target2);
+	printf("ER: dup addr |%p|\n", target2);
+    free(target2);
+    char *target3 = ft_strdup(source1);
+    if (target3 == NULL) {
+        printf("AR:Memory allocation failed.\n");
+        return 1;
+    }
+    printf("AR:Duplicated string: %s\n", target3);
+	printf("AR: dup addr |%p|\n", target3);
+    free(target3);
 }
